b30d41194d246e80c05a2955d6a3c5b820d2cff4
Dreamwalker <aristojeff@gmail.com>
2020-04-15 17:18:41 +0900

    add add method

--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exists")
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -13,4 +14,14 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+func (d Dictionary) Add(word, def string) err {
+	_, err := d.Search(word)
+	if err == errNotFound {
+		d[word] = def
+	} else if err == nil {
+		return errWordExists
+	}
+	return nil
 }
