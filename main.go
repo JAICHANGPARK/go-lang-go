bbdf92d72c2573375f9626f8e6d4372a5588ffe9
Dreamwalker <aristojeff@gmail.com>
2020-04-18 19:34:27 +0900

    update main code

--- main.go
+++ main.go
@@ -1,34 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/JAICHANGPARK/learngo/mydict"
-)
-
 func main() {
-	// fmt.Println("Hello")
-	// acc := banking.NewAccount("Dream")
-	// fmt.Println(acc)
-	// acc.Deposit(10)
-	// fmt.Println(acc.Balance())
-	// err := acc.Withdraw(20)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println(acc.Balance(), acc.Onwer())
 
-	dictionary := mydict.Dictionary{"name": "Dreamwalker"}
-	dictionary["hello"] = "Hello"
-	fmt.Println(dictionary)
-	definition, err := dictionary.Search("second")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
-	}
-	errs := dictionary.Add("hello", "World")
-	if errs != nil {
-		fmt.Println(errs)
-	}
 }
