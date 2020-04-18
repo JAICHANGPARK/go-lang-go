9b9aca9f1c1220cb263a975ab7ea5a5e7889ad94
Dreamwalker <aristojeff@gmail.com>
2020-04-18 19:41:07 +0900

    add http get request

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request Failed")
+
 func main() {
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+	}
+	for _, url := range urls {
+		fmt.Println(url)
+	}
+}
+
+func hitURL(url string) error {
+
+	resp, err := http.Get(url)
+	if err != nil {
+
+	}
+	if resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
 
 }
